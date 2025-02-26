e4e8bac74c8fcecaa8baf784f4dc14df257ad839
rami matarneh <ramimatarneh310@outlook.com>
2025-02-26 10:39:49 +0300

    last update

--- OceanaAura.Web/Models/Auth/LoginVM.cs
+++ OceanaAura.Web/Models/Auth/LoginVM.cs
@@ -13,16 +13,14 @@ namespace OceanaAura.Web.Models.Auth
         public LoginVMValidator()
         {
             RuleFor(x => x.Email)
-            .NotEmpty().WithMessage("Email is required.")
-            .Matches(@"^[a-zA-Z0-9._%+-]+@gmail\.com$").WithMessage("Only Gmail addresses are allowed.");
+                .Matches(@"^[a-zA-Z0-9._%+-]+@(gmail\.com|outlook\.com|yahoo\.com|hotmail\.com)$")
+                .WithMessage("Please provide a valid Gmail, Outlook, Yahoo, or Hotmail address.");
 
             RuleFor(user => user.Password)
               .NotEmpty().WithMessage("Password is required")
               .MinimumLength(6).WithMessage("Password must be at least 6 characters long")
               .Matches(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{6,}$")
               .WithMessage("Password must contain at least one uppercase letter, one lowercase letter, one number, and one special character.");
-
-
         }
     }
 }
